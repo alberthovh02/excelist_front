e1cffd910611aab9fdafd6571629c8119e1e75a5
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-02-07 11:48:51 +0400

    Footer style

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -41,6 +41,7 @@ class Footer extends React.Component{
                         <p className="footer-info">ք. Երևան, Արշակունյաց 2` «Տիգրան Մեծ» հրատարակչություն, 3-րդ հարկ</p>
                         </div>
                         <div className="footer-social-icons">
+                        <a href="https://www.facebook.com/Excel.lessons/?fref=ts" target="_blank">
                           <i className="fa fa-facebook" style={{
           									backgroundColor: "#3b5998",
           									fontSize: "18px",
@@ -48,6 +49,8 @@ class Footer extends React.Component{
           									borderRadius: "100px",
           									color: "white"
           								}}></i>
+                          </a>
+                          <a href="https://www.youtube.com/channel/UCIhWQ4k5FSaXrn8uKuLin7A" target="_blank">
                           <i className="fa fa-youtube" style={{
                             backgroundColor: "red",
                             fontSize: "18px",
@@ -55,6 +58,8 @@ class Footer extends React.Component{
                             borderRadius: "100px",
                             color: "white"
                           }}></i>
+                          </a>
+                          <a href="https://www.linkedin.com/company/13211031?trk=tyah&trkInfo=clickedVertical%3Acompany%2CclickedEntityId%3A13211031%2Cidx%3A1-1-1%2CtarId%3A1474012711640%2Ctas%3A%D4%B7%D6%84%D5%BD" target="_blank">
                           <i className="fa fa-linkedin" style={{
                             backgroundColor: "#0077B5",
                             fontSize: "18px",
@@ -62,6 +67,7 @@ class Footer extends React.Component{
                             borderRadius: "100px",
                             color: "white"
                           }}></i>
+                          </a>
                           <i className="fa fa-edit" style={{
                             backgroundColor: "#72c752",
                             fontSize: "18px",
