1b65daa66cd699d14eb0336ed905297323cc04e0
alberthovh02 <albert.hovhannisyan.main@gmail.com>
2020-06-08 21:14:32 +0400

    Fixed registratoin form

--- src/app/components/Register.js
+++ src/app/components/Register.js
@@ -5,6 +5,11 @@ import Footer from './Footer';
 
 export default function Register(){
   return(
-    <><Header/><div style={{marginBottom: 100, display: "flex", justifyContent: "center"}}><iframe src="https://docs.google.com/forms/u/0/d/e/1FAIpQLSdeUlcWwLVjDouBRR-MwGby_sUOAQMsfGrl-cdi5rmsXlvQ5w/formResponse?embedded=true" width="640" height="800" frameBorder="0" marginHeight="0" marginWidth="0">Загрузка…</iframe></div><Footer mode="simple"/></>
+    <><Header/><div style={{marginBottom: 100, display: "flex", justifyContent: "center"}}>
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSeNFP3JBVzQGsNV_Bjd0wRq4LT7PETc23dsmpGMNNk6He5hjA/viewform?embedded=true"
+            width="700" height="520" frameBorder="0" marginHeight="0" marginWidth="0">Загрузка…
+        </iframe>
+      </div><Footer mode="simple"/></>
   )
 }
