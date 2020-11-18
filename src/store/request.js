59fc8f4514f52c472576405e4fce224a6c1d00a2
Albert Hovhannisyan <albert.hovhannisyan002@gmail.com>
2020-11-18 12:00:21 +0400

    Adding api/vi to main route

--- src/store/request.js
+++ src/store/request.js
@@ -1,6 +1,6 @@
 class Request {
   constructor(){
-    this.mainRoute = "//159.65.216.209:5000/"
+    this.mainRoute = "//159.65.216.209:5000/api/v1/"
     // this.mainRoute = 'https://excelist.tk:5000/'
     // this.mainRoute = 'https://excelist.am/api/v1/'
   }
