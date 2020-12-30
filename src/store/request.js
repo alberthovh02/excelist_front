26fe888884a03edffb1e3cc186fbc8033d069ab9
vahe-nikoghosyan <vahenikoghosyan.97@gmail.com>
2020-12-30 05:01:18 -0800

    d

--- src/store/request.js
+++ src/store/request.js
@@ -1,37 +1,43 @@
 class Request {
-  constructor(){
-    this.mainRoute = "http://159.65.216.209:5000/api/v1/"
+  constructor() {
+    // this.mainRoute = "http://159.65.216.209:5000/api/v1/"
     // this.mainRoute = 'https://excelist.tk:5000/'
-    // this.mainRoute = 'https://excelist.am/api/v1/'
+    this.mainRoute = "https://api.excelist.am/api/v1/";
   }
 
-  get = url => this.sendRequest("GET", url);
+  get = (url) => this.sendRequest("GET", url);
 
-  post = (url, data) => this.sendRequest('POST', url, data, null);
+  post = (url, data) => this.sendRequest("POST", url, data, null);
 
-  postJson = (url, data) => this.sendRequest('POST', url, JSON.stringify(data), 'application/json');
+  postJson = (url, data) =>
+    this.sendRequest("POST", url, JSON.stringify(data), "application/json");
 
-  postFormData = (url, data) => this.sendRequest("POST", url, data)
+  postFormData = (url, data) => this.sendRequest("POST", url, data);
 
-  postJson = (url, data) => this.sendRequest("POST", url, JSON.stringify(data), 'application/json');
+  postJson = (url, data) =>
+    this.sendRequest("POST", url, JSON.stringify(data), "application/json");
 
-  put = (url, data) => this.sendRequest("PUT", url, JSON.stringify(data), 'application/json')
+  put = (url, data) =>
+    this.sendRequest("PUT", url, JSON.stringify(data), "application/json");
 
   putFormData = (url, data) => this.sendRequest("PUT", url, data);
 
-  delete = url => this.sendRequest("DELETE", url);
+  delete = (url) => this.sendRequest("DELETE", url);
 
-  sendRequest(method, url, data, contentType){
+  sendRequest(method, url, data, contentType) {
     const token = localStorage.getItem("authorizedUser");
     let headers = {};
     contentType
-      ? (headers = { 'Content-Type': contentType, Authorization: 'Bearer ' + token})
-      : (headers = { Authorization: "Bearer " + token});
-      return fetch(this.mainRoute + url, {
-        method,
-        headers,
-        body: data
-      })
+      ? (headers = {
+          "Content-Type": contentType,
+          Authorization: "Bearer " + token,
+        })
+      : (headers = { Authorization: "Bearer " + token });
+    return fetch(this.mainRoute + url, {
+      method,
+      headers,
+      body: data,
+    });
   }
 }
 
